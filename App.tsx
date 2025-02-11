eea9f8ad67d3f37498ffb8a26f77a0d3d0988f21
mohan <2948579997@qq.com>
2025-02-11 22:02:57 +0800

    1.更改了字重。2.精简了暂无待办时的动画的代码。

---
+++ App.tsx
@@ -0,0 +1,99 @@
+import { Redirect, Route } from "react-router-dom";
+import {
+  IonApp,
+  IonIcon,
+  IonLabel,
+  IonRouterOutlet,
+  IonTabBar,
+  IonTabButton,
+  IonTabs,
+  setupIonicReact,
+} from "@ionic/react";
+import { IonReactRouter } from "@ionic/react-router";
+import Home from "./pages/Home";
+import Tab2 from "./pages/Schedule";
+import Tab3 from "./pages/Todos";
+import home from "./footerLogo/home.svg";
+import schedule from "./footerLogo/schedule.svg";
+import todo from "./footerLogo/todos.svg";
+import functions from "./footerLogo/functions.svg";
+import mine from "./footerLogo/mine.svg";
+
+/* Core CSS required for Ionic components to work properly */
+import "@ionic/react/css/core.css";
+
+/* Basic CSS for apps built with Ionic */
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
+
+/* Optional CSS utils that can be commented out */
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
+
+/**
+ * Ionic Dark Mode
+ * -----------------------------------------------------
+ * For more info, please see:
+ * https://ionicframework.com/docs/theming/dark-mode
+ */
+
+/* import '@ionic/react/css/palettes/dark.always.css'; */
+/* import '@ionic/react/css/palettes/dark.class.css'; */
+import "@ionic/react/css/palettes/dark.system.css";
+
+/* Theme variables */
+import "./theme/variables.css";
+
+setupIonicReact();
+
+const App: React.FC = () => (
+  <IonApp>
+    <IonReactRouter>
+      <IonTabs>
+        <IonRouterOutlet>
+          <Route exact path="/home">
+            <Home />
+          </Route>
+          <Route exact path="/schedule">
+            <Tab2 />
+          </Route>
+          <Route path="/todos">
+            <Tab3 />
+          </Route>
+          <Route exact path="/">
+            <Redirect to="/home" />
+          </Route>
+        </IonRouterOutlet>
+        <IonTabBar slot="bottom">
+          <IonTabButton tab="tab1" href="/home">
+            <IonIcon aria-hidden="true" icon={home} />
+            <IonLabel>主页</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="tab2" href="/schedule">
+            <IonIcon aria-hidden="true" icon={schedule} />
+            <IonLabel>日程</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="tab3" href="/todos">
+            <IonIcon aria-hidden="true" icon={todo} />
+            <IonLabel>待办</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="tab4" href="/functions">
+            <IonIcon aria-hidden="true" icon={functions} />
+            <IonLabel>功能</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="tab5" href="/mine">
+            <IonIcon aria-hidden="true" icon={mine} />
+            <IonLabel>我的</IonLabel>
+          </IonTabButton>
+        </IonTabBar>
+      </IonTabs>
+    </IonReactRouter>
+  </IonApp>
+);
+
+export default App;
