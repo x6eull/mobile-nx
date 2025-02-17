db77654fe41733ae663f4b22f060bba6c3924712
mohan-zeyu <2948579997@qq.com>
2025-02-17 21:31:16 +0800

    main.tsx update

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import App from './App'
+import { initCapacitorApp } from './interop'
 
-const container = document.getElementById('root')
-const root = createRoot(container!)
-root.render(
+initCapacitorApp()
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
