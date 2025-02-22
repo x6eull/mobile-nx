671298587bd72200e019d32e3e1a0064cc8f6ed8
serein6174 <serein6174@163.com>
2025-02-22 10:52:18 +0800

    changetheapproach

--- src/HomeworkSpider.ts
+++ src/HomeworkSpider.ts
@@ -1,21 +1,16 @@
-import { config } from 'dotenv'
-config({ path: '.env.local' })
-
-import { ZjuamService } from './src/interop/zjuam'
+import { ZjuamService } from './interop/zjuam'
 
 /** 学在浙大作业
-    courseid:课程id
+    courseId:课程id
     deadline:截止日期
     title:作业名字
-    isClosed:作业是否截止
-    submitted:用户是否提交
+    type:日程类型homework/exam/questionnaire
  */
 export interface Schedule {
   courseId: number
   deadline: string
   title: string
-  isClosed: boolean
-  submitted: boolean
+  type: string
 }
 export class HomeworkSpider {
   zjuamService: ZjuamService
@@ -26,42 +21,22 @@ export class HomeworkSpider {
     )
   }
   private homework: Schedule[] = []
-  /**获取所有作业*/
+  /**获取所有作业和考试*/
   async fetchHomework(): Promise<Schedule[] | undefined> {
-    const url: string = 'https://courses.zju.edu.cn/api/my-courses'
+    let url: string = 'https://courses.zju.edu.cn/api/todos'
     const response = await this.zjuamService.nxFetch.get(url)
     const data = await response.json()
-    for (let i: number = 0; i < data.courses.length; i++) {
-      if (data.courses[i].is_closed === true) continue
-      let url2: string =
-        'https://courses.zju.edu.cn/api/courses/' +
-        String(data.courses[i].id) +
-        '/homework-activities'
-      await this.fetchHomeworkByEachId(url2, data.courses[i].id)
-    }
-    return this.homework
-  }
-  /**获取单个课程下的所有作业,id为该课程在学在浙大的id*/
-  async fetchHomeworkByEachId(
-    url: string,
-    id: number,
-  ): Promise<Schedule[] | undefined> {
-    let homework: Schedule[] = []
-    const response = await this.zjuamService.nxFetch.get(url)
-    const data = await response.json()
-    for (let i = 0; i < data.homework_activities.length; i++) {
-      const value = data.homework_activities[i]
-      if (value.is_closed === true) continue
+    for (let i = 0; i < data.todo_list.length; i++) {
+      const value = data.todo_list[i]
+      console.log(value)
       this.homework.push({
-        courseId: id,
-        deadline: value.deadline,
+        courseId: value.course_id,
+        deadline: value.end_time,
         title: value.title,
-        isClosed: value.is_closed,
-        submitted: value.submitted,
+        type: value.type,
       })
     }
-
-    return homework
+    return this.homework
   }
 }
 
