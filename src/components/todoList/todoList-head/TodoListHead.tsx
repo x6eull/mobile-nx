44c7c94012d0c442a576d65bd4d40e1ac3b6d00d
mohan-zeyu <2948579997@qq.com>
2025-03-01 10:44:18 +0800

    change the color of todo-icon

--- src/components/todoList/todoList-head/TodoListHead.tsx
+++ src/components/todoList/todoList-head/TodoListHead.tsx
@@ -3,8 +3,7 @@ import './TodoListHead.css'
 function TodoHead() {
   return (
     <div className="todo-head">
-      <object type="image/svg+xml" data={logo} className="img"></object>
-      {/* <img src={logo} alt="404" /> */}
+      <img src={logo} alt="404" />
       <div className="todo-head-head">待办事项</div>
       <a href="/todos" className="todo-head-all">
         查看全部 {'>'}
