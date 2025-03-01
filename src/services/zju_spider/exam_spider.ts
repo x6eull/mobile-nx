15ad0cd26f91cb94d8ae917710bbff94ea1fca11
5dbwat4 <me@5dbwat4.top>
2025-03-01 14:30:56 +0800

    Update exam_spider.ts

--- src/services/zju_spider/exam_spider.ts
+++ src/services/zju_spider/exam_spider.ts
@@ -17,28 +17,30 @@ interface fullExamDataItem {
   seat: number
 }
 
-/**
- *
- * @param str looks like '2025年01月04日(14:00-16:00)'
- */
-const parseZDBKDate = (str: string) => {
-  const slice = str
-    .split(/年|月|日|\(|\)|:|-/)
-    .filter((v: any) => v)
-    .map(Number)
-  return {
-    startAt: new Date(
-      slice[0],
-      slice[1] - 1,
-      slice[2],
-      slice[3],
-      slice[4],
-      0,
-      0,
-    ),
-    endAt: new Date(slice[0], slice[1] - 1, slice[2], slice[5], slice[6], 0, 0),
-  }
-}
+// /**
+//  *
+//  * @param str looks like '2025年01月04日(14:00-16:00)'
+//  */
+// const parseZDBKDate = (str: string) => {
+//   const slice = str
+//     .split(/年|月|日|\(|\)|:|-/)
+//     .filter((v: any) => v)
+//     .map(Number)
+//   return {
+//     startAt: new Date(
+//       slice[0],
+//       slice[1] - 1,
+//       slice[2],
+//       slice[3],
+//       slice[4],
+//       0,
+//       0,
+//     ),
+//     endAt: new Date(slice[0], slice[1] - 1, slice[2], slice[5], slice[6], 0, 0),
+//   }
+// }
+
+import { parseZDBKDate } from '@/utils/parseZDBKDate'
 
 // /**
 //  * @param custom_fetch WIP, a fetch function that handles cookies automatically
