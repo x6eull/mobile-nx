3a426f0cbdc876e0633a35e7d300958b52bea1aa
zjukunk <3057347288@qq.com>
2025-03-07 20:45:12 +0800

    主要解决上传到git之后出现的报错

--- src/getcourse.ts
+++ src/getcourse.ts
@@ -36,30 +36,28 @@ class GetCourse {
     const seenCourses = new Set<string>() // 用于存储已经处理过的课程的唯一标识
 
     for (let i = 0; i < courseList.length; i++) {
-      let currentCourse = courseList[i]
+      const currentCourse = courseList[i]
+      const currentCourseInfo = currentCourse.classes[0]
 
       // 检查是否与下一个课程完全相同
       while (
         i + 1 < courseList.length &&
         currentCourse.id === courseList[i + 1].id &&
-        currentCourse.classes[0].location ===
-          courseList[i + 1].classes[0].location &&
-        currentCourse.classes[0].dayOfWeek ===
+        currentCourseInfo.location === courseList[i + 1].classes[0].location &&
+        currentCourseInfo.dayOfWeek ===
           courseList[i + 1].classes[0].dayOfWeek &&
-        currentCourse.classes[0].weekType ===
-          courseList[i + 1].classes[0].weekType &&
-        currentCourse.classes[0].startSection +
-          currentCourse.classes[0].sectionCount ===
+        currentCourseInfo.weekType === courseList[i + 1].classes[0].weekType &&
+        currentCourseInfo.startSection + currentCourseInfo.sectionCount ===
           courseList[i + 1].classes[0].startSection
       ) {
         // 合并连续的课程
-        currentCourse.classes[0].sectionCount +=
+        currentCourseInfo.sectionCount +=
           courseList[i + 1].classes[0].sectionCount
         i++
       }
 
       // 生成课程的唯一标识
-      const courseKey = `${currentCourse.id}-${currentCourse.classes[0].dayOfWeek}-${currentCourse.classes[0].startSection}-${currentCourse.classes[0].location}-${currentCourse.classes[0].weekType}-${currentCourse.classes[0].sectionCount}`
+      const courseKey = `${currentCourse.id}-${currentCourseInfo.dayOfWeek}-${currentCourseInfo.startSection}-${currentCourseInfo.location}-${currentCourseInfo.weekType}-${currentCourseInfo.sectionCount}`
 
       // 如果这个课程还没有被处理过，则加入到结果中
       if (!seenCourses.has(courseKey)) {
@@ -88,26 +86,18 @@ class GetCourse {
   }
 
   /** 提取课程信息的函数，主要作用是对返回的数据进行处理，并转换成我们需要的course格式  */
-  private async extractClassInfo(data: {
+  private extractClassInfo(data: {
     kbList: {
-      /**传递了一串具体的课程信息，后续需要处理来获得需要内容*/
       kcb: string
-      /**单双周表示，0 1 2分别代表单周 双周 每周*/
       dsz: string
-      /**第几节课开始*/
       djj: string
-      /**星期几*/
       xqj: number
-      /**学期区，用汉字标注了经过那几个学期*/
       xxq: string
-      /**选课课号，也就是课程ID*/
       xkkh: string
-      /**上课长度，用数字标注了上课的长度*/
       skcd: string
     }[]
-    /*学年名,如2021-2022，后续仅保留前面即可*/
     xnm: string
-  }): Promise<Course[]> {
+  }): Course[] {
     const classInfo: Course[] = []
 
     if (!data || !data.kbList || !Array.isArray(data.kbList)) {
@@ -131,9 +121,7 @@ class GetCourse {
       const classTeacherName = kcbItem[2]
       let classLocation = kcbItem[3]
 
-      if (classLocation.includes('zwf')) {
-        classLocation = classLocation.split('zwf')[0].trim()
-      }
+      classLocation = classLocation.replace(/zwf.*/, '').trim()
 
       const termIdMap = {
         春: Term.Spring,
@@ -149,6 +137,8 @@ class GetCourse {
         const season = xxq[j]
         if (season in termIdMap) {
           termId |= termIdMap[season as keyof typeof termIdMap]
+        } else {
+          throw new Error(`学期匹配失败`)
         }
       }
 
@@ -185,9 +175,9 @@ class GetCourse {
   async getTimetable(
     /** 学号  */
     userid: string,
-    /** 起始学年（靠前的，如2024-2025请传2025）请传字符串！！！ */
+    /** 起始学年（靠前的，如2024-2025请传2024）请传字符串！！！ */
     xnmStart: string,
-    /** 结束学年（靠前的，如2024-2025请传2025） 请传字符串！！！*/
+    /** 结束学年（靠前的，如2024-2025请传2024） 请传字符串！！！*/
     xnmEnd: string,
   ): Promise<Course[]> {
     const url = `http://zdbk.zju.edu.cn/jwglxt/kbcx/xskbcx_cxXsKb.html?gnmkdm=N253508&su=${userid}`
@@ -198,7 +188,7 @@ class GetCourse {
       { xqm: '1|冬', xqmmc: '冬' },
       { xqm: '2|春', xqmmc: '春' },
       { xqm: '2|夏', xqmmc: '夏' },
-      { xqm: '1|长', xqmmc: '长' },
+      { xqm: '2|短', xqmmc: '长' }, //教务网有两个短，现在将秋冬学期的称为短学期，春夏学期的称为长学期
       { xqm: '1|短', xqmmc: '短' },
       { xqm: '1|暑', xqmmc: '暑' },
     ]
@@ -235,10 +225,7 @@ class GetCourse {
         const uniqueClassInfo = this.removeDuplicates(classInfo)
 
         // 合并课程信息
-        allCourses = await this.mergeCourses([
-          ...allCourses,
-          ...uniqueClassInfo,
-        ])
+        allCourses = this.mergeCourses([...allCourses, ...uniqueClassInfo])
       }
     }
 
