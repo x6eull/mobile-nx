787c5269db0ebb9a663e69b37c70c8630c506dc7
zjukunk <3057347288@qq.com>
2025-03-09 09:19:59 +0800

    不小心传了不该传的文件

--- src/utils/extendHeaders.js
+++
@@ -1,6 +0,0 @@
-Object.defineProperty(Headers.prototype, 'setDefault', {
-    value: function (key, defaultValue) {
-        if (!this.has(key))
-            this.set(key, defaultValue);
-    },
-});
