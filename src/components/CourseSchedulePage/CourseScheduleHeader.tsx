08a9d5ac64f0d64c79a8b48060b1b757583acc00
Jocelyn <3759323092@qq.com>
2025-03-15 21:19:22 +0800

    css

--- src/components/CourseSchedulePage/CourseScheduleHeader.tsx
+++ src/components/CourseSchedulePage/CourseScheduleHeader.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
-import { IonButton, IonImg } from '@ionic/react'
+import { IonButton, IonImg, useIonToast } from '@ionic/react'
 import eye from '../../assets/kbheader-eye.png'
 import out from '../../assets/kbheader-out.png'
+import toast from '../../assets/toast.jpg'
+
 import './CourseScheduleHeader.css'
 
 interface CourseScheduleHeaderProps {
@@ -15,8 +17,33 @@ const CourseScheduleHeader: React.FC<CourseScheduleHeaderProps> = ({
   isTextVisible,
   onTextVisibilityChange,
 }) => {
+  const [present] = useIonToast()
+
   const handleEyeClick = () => {
     onTextVisibilityChange(!isTextVisible)
+  }
+
+  const showToast = (success: boolean) => {
+    present({
+      message: success ? '保存图片成功！' : '保存图片失败！',
+      duration: 2000,
+      position: 'top',
+      cssClass: 'custom-toast',
+      image: toast,
+    })
+  }
+
+  const handleSaveClick = () => {
+    try {
+      // 这里添加保存图片的逻辑
+      // ...
+
+      // 成功后显示成功提示
+      showToast(true)
+    } catch (error) {
+      showToast(false)
+      console.error('保存失败:', error)
+    }
   }
 
   return (
@@ -29,7 +56,7 @@ const CourseScheduleHeader: React.FC<CourseScheduleHeaderProps> = ({
         <IonButton fill="clear" size="small" onClick={handleEyeClick}>
           <IonImg src={eye} alt="eye" />
         </IonButton>
-        <IonButton fill="clear" size="small">
+        <IonButton fill="clear" size="small" onClick={handleSaveClick}>
           <IonImg src={out} alt="Out" />
         </IonButton>
       </div>
